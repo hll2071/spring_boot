db611a88161b546b6a272dc8c70629b139fedb4f
hsm <hll2071@naver.com>
2024-11-14 10:50:46 +0900

    Mapping

---
+++ src/main/java/shopping/shop/BasicController.java
@@ -0,0 +1,40 @@
+package shopping.shop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class BasicController {
+    @GetMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "안녕하세용";
+    }
+
+    @GetMapping("/about")
+    @ResponseBody
+    public String about() {
+        return "피싱 사이트에용";
+    }
+
+    @GetMapping("/html")
+    @ResponseBody
+    public String html() {
+        return "<h4>반갑습니다</h4>";
+    }
+
+    @GetMapping("/html1")
+    public String html1() {
+        return "index.html";
+    }
+
+    @GetMapping("/date")
+    @ResponseBody
+    public String date() {
+        return LocalDateTime.now().toString();
+    }
+}
